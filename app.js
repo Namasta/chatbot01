c9e0b973308949e9c74f984b409d48cf6da56ebb
Namasta <fcchoong@gmail.com>
2018-12-04 19:36:43 +0800

    Newly check in

--- app.js
+++ app.js
@@ -1,22 +1,53 @@
 //This section list the required libraries
 var express = require("express");
+var bodyParser = require('body-parser');
+var functions = require('firebase-functions');
+var {WebhookClient,Card,} = require('dialogflow-fulfillment');
+
 //Create an instance of express server
-var app = express();
+var expressApp = express().use(bodyParser.json());
 
-app.get("/",function(req,res){
-    res.send('<h1>This is my web app</h1>');
-});
+//const express = require('express');
+//const bodyParser = require('body-parser');
+//const functions = require('firebase-functions');
+//const {WebhookClient,Card,} = require('dialogflow-fulfillment');
+
+//const expressApp = express().use(bodyParser.json());
+expressApp.post('/fulfillment', functions.https.onRequest((request, response) => {
+ const agent = new WebhookClient({ request, response });
+  function welcome(agent) {
+   agent.add(`Welcome to my agent in Heroku!`);
+   agent.add(new Card({
+       title: `This is Agent in Heroku`,
+       imageUrl: 'http://weknowyourdreams.com/images/robot/robot-02.jpg',
+       text: `I am here to serve you.\nPlease free to ask me anything! 💁`,
+       buttonText: 'Click Me to know more about me!',
+       buttonUrl: 'https://assistant.google.com/'
+     })
+   );
+ }
+ function fallback(agent) {
+   agent.add(`I didn't understand`);
+   agent.add(`I'm sorry, can you try again?`);
+ }
 
 
-app.get("/something",function(req,res){
-    res.send('<h1>This is something</h1>');
-});
+ // Run the proper function handler based on the matched Dialogflow intent name
+ let intentMap = new Map();
+ intentMap.set('Default Welcome Intent', welcome);
+ intentMap.set('Default Fallback Intent', fallback);
+ agent.handleRequest(intentMap);
+})
+);
 
-//Start the express server to listen to a port in the server
-var listener = app.listen(
-   process.env.PORT,
-   process.env.IP,
-   function(){
-    console.log("server has started");
-    console.log('Listening on port ' + listener.address().port);
-});
+
+var listener =
+expressApp.listen(
+       process.env.PORT,
+       process.env.IP,
+       function(){
+           console.log("server started");
+           console.log("listening on port " +
+           listener.address().port);
+       });
+
